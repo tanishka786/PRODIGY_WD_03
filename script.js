38b2bbca840e88878a311e5b1f0121a13040fdcd
tanishka786 <tanishkajdas@gmail.com>
2023-12-31 01:43:28 -0400

    show current player now

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const vsButtons = document.querySelector('.vs-button');
-const twoPlayers = document.getElementById('twoPlayers');
-const vsComputer = document.getElementById('vsComputer');
-const twoPlayersSection = document.querySelector('.two-players');
-const vsComputerSection = document.querySelector('.vs-computer');
-const restartButton = document.getElementById('restartButton')
+const vsButtons = document.querySelector(".vs-button");
+const twoPlayers = document.getElementById("twoPlayers");
+const vsComputer = document.getElementById("vsComputer");
+const twoPlayersSection = document.querySelector(".two-players");
+const vsComputerSection = document.querySelector(".vs-computer");
+const restartButton = document.getElementById("restartButton");
+const currentPlayerText = document.querySelector("#current-player");
+
+let currentPlayer = "X";
+let board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+];
+let gameOver = false;
 
 function twoPlayersGame() {
     twoPlayersSection.style.display = "";
     vsComputerSection.remove();
     vsButtons.style.display = "none";
     restartButton.style.display = "";
+    currentPlayerText.innerHTML = currentPlayer;
 }
-twoPlayers.addEventListener('click', twoPlayersGame);
+twoPlayers.addEventListener("click", twoPlayersGame);
 
 function vsComputerGame() {
     vsComputerSection.style.display = "";
@@ -19,51 +29,56 @@ function vsComputerGame() {
     vsButtons.style.display = "none";
     restartButton.style.display = "";
 }
-vsComputer.addEventListener('click', vsComputerGame);
-
-
-let currentPlayer = 'X';
-let board = [['', '', ''], ['', '', ''], ['', '', '']];
-let gameOver = false;
+vsComputer.addEventListener("click", vsComputerGame);
 
 function twoPMakeMove(row, col) {
-    if (!gameOver && board[row][col] === '') {
+    if (!gameOver && board[row][col] === "") {
         board[row][col] = currentPlayer;
-        document.querySelector(`.row:nth-child(${row + 1}) .cell:nth-child(${col + 1}`).textContent = currentPlayer;
-        
+        document.querySelector(
+            `.row:nth-child(${row + 1}) .cell:nth-child(${col + 1}`,
+        ).textContent = currentPlayer;
+
         if (checkWin()) {
-            document.getElementById('twoPlayersWinnerText').textContent = `${currentPlayer} wins!`;
+            document.getElementById(
+                "twoPlayersWinnerText",
+            ).textContent = `${currentPlayer} wins!`;
             gameOver = true;
         } else if (checkTie()) {
-            document.getElementById('twoPlayersWinnerText').textContent = "It's a tie!";
+            document.getElementById("twoPlayersWinnerText").textContent =
+                "It's a tie!";
             gameOver = true;
         } else {
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+            currentPlayerText.innerHTML = currentPlayer;
         }
     }
 }
 
 function vsComMakeMove(row, col) {
-    if (!gameOver && board[row][col] === '') {
+    if (!gameOver && board[row][col] === "") {
         board[row][col] = currentPlayer;
-        document.querySelector(`.row:nth-child(${row + 1}) .cell:nth-child(${col + 1}`).textContent = currentPlayer;
+        document.querySelector(
+            `.row:nth-child(${row + 1}) .cell:nth-child(${col + 1}`,
+        ).textContent = currentPlayer;
 
         if (checkWin()) {
-            document.getElementById('vsComputerWinnerText').textContent = `${currentPlayer} wins!`;
+            document.getElementById(
+                "vsComputerWinnerText",
+            ).textContent = `${currentPlayer} wins!`;
             gameOver = true;
         } else if (checkTie()) {
-            document.getElementById('vsComputerWinnerText').textContent = "It's a tie!";
+            document.getElementById("vsComputerWinnerText").textContent =
+                "It's a tie!";
             gameOver = true;
         } else {
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
 
-            if (!gameOver && currentPlayer === 'O') {
-                setTimeout(makeComputerMove, 500); 
+            if (!gameOver && currentPlayer === "O") {
+                setTimeout(makeComputerMove, 500);
             }
         }
     }
 }
-
 
 function makeComputerMove() {
     if (!gameOver) {
@@ -71,8 +86,8 @@ function makeComputerMove() {
         do {
             row = Math.floor(Math.random() * 3);
             col = Math.floor(Math.random() * 3);
-        } while (board[row][col] !== '');
-        
+        } while (board[row][col] !== "");
+
         vsComMakeMove(row, col);
     }
 }
@@ -80,26 +95,34 @@ function makeComputerMove() {
 function checkWin() {
     for (let i = 0; i < 3; i++) {
         if (
-            (board[i][0] === currentPlayer && board[i][1] === currentPlayer && board[i][2] === currentPlayer) ||
-            (board[0][i] === currentPlayer && board[1][i] === currentPlayer && board[2][i] === currentPlayer)
+            (board[i][0] === currentPlayer &&
+                board[i][1] === currentPlayer &&
+                board[i][2] === currentPlayer) ||
+            (board[0][i] === currentPlayer &&
+                board[1][i] === currentPlayer &&
+                board[2][i] === currentPlayer)
         ) {
             return true;
         }
     }
-    
+
     if (
-        (board[0][0] === currentPlayer && board[1][1] === currentPlayer && board[2][2] === currentPlayer) ||
-        (board[0][2] === currentPlayer && board[1][1] === currentPlayer && board[2][0] === currentPlayer)
+        (board[0][0] === currentPlayer &&
+            board[1][1] === currentPlayer &&
+            board[2][2] === currentPlayer) ||
+        (board[0][2] === currentPlayer &&
+            board[1][1] === currentPlayer &&
+            board[2][0] === currentPlayer)
     ) {
         return true;
     }
-    
+
     return false;
 }
 
 function checkTie() {
     for (let row of board) {
-        if (row.includes('')) {
+        if (row.includes("")) {
             return false;
         }
     }
@@ -109,4 +132,4 @@ function checkTie() {
 function restartGame() {
     location.reload();
 }
-restartButton.addEventListener('click', restartGame);
+restartButton.addEventListener("click", restartGame);
